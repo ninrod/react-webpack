2906a41c80fbc3426cf784b9db284f3cafc53b08
ninrod <filipe.silva@gmail.com>
2019-05-02 23:21:34 -0300

    optimize webpack config: remove prod configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     host: '0.0.0.0',
-    port: '8080'
+    port: '4200'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
